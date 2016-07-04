285d2db721f551e80be1edd5e60cbafc5e04f6d5
Tim Proffitt <tim.proffitt@gmail.com>
2016-07-04 17:02:22 -0500

    solution for v1.0

--- client/app/components/shop/shop-itemdetail.directive.js
+++ client/app/components/shop/shop-itemdetail.directive.js
@@ -8,10 +8,25 @@
     		template: '<h4>Item Detail</h4>' +
                         '<span ng-show="!equipment.length > 0" class="glyphicon glyphicon-refresh glyphicon-spin"></span>' +
                         '<ul class="list-unstyled">'+
-                            '<li>Show item detail here</li>' +
+                            '<li>{{selectedItem.name}}</li>' +
+                            '<li>{{selectedItem.description}}</li>' +
                         '</ul>',
     		link: function(scope) {
+                scope.resource = dataservice;
+                scope.equipment = scope.resource.equipment;
+                scope.selectedItem = null;
 
+                setSelected();
+
+                scope.$watch('equipment', function(newVal, oldVal) {
+                    setSelected();
+                }, true);
+
+                function setSelected() {
+                    scope.selectedItem = scope.equipment.find(function(item) {
+                        return item.selected===true;
+                    });
+                }
             }
     	};
 
--- client/app/components/shop/shop-itemlist.directive.js
+++ client/app/components/shop/shop-itemlist.directive.js
@@ -8,12 +8,27 @@
     		template: '<h4>item list</h4>' +
     					'<span ng-show="!equipment.length > 0" class="glyphicon glyphicon-refresh glyphicon-spin"></span>' +
   						'<ul class="list-unstyled">' +
-					        '<li ng-repeat="item in equipment"><a>{{item.name}}</a></li>' +
+					        '<li ng-repeat="item in equipment"><a ng-click="selectItem(item)">{{item.name}}</a></li>' +
 					    '</ul>',
     		link: function(scope) {
+
+                scope.selectItem = selectItem;
     			dataservice.getEquipment().then(function(data) {
                     scope.equipment = data;
+                    scope.selectItem();
                 });
+
+                function selectItem(item) {
+                    if(!item) {
+                        item = scope.equipment[0];
+                    } else {
+                        scope.equipment.forEach(function(i) {
+                            i.selected = false;
+                        });
+                    }
+                    item.selected = true;
+                }
+                
     		}
     	};
 
