34d5da73850c75984e9178230ce11fc06756a2e3
Tim Proffitt <tim.proffitt@gmail.com>
2016-07-04 16:27:05 -0500

    git obfuscatemsg a&b-cx2nng-es+9+q

--- client/app/components/shop/shop-itemdetail.directive.js
+++ client/app/components/shop/shop-itemdetail.directive.js
@@ -2,36 +2,16 @@
   'use strict';
 
   angular.module('App.components.shop')
-    .directive('itemdetail', ['dataservice', function (dataservice, $q) {
+    .directive('itemdetail', ['dataservice', function (dataservice) {
 
     	var ddo = {
     		template: '<h4>Item Detail</h4>' +
                         '<span ng-show="!equipment.length > 0" class="glyphicon glyphicon-refresh glyphicon-spin"></span>' +
                         '<ul class="list-unstyled">'+
-                            '<li>{{selectedItem.name}}</li>' +
-                            '<li>{{selectedItem.description}}</li>' +
+                            '<li>Show item detail here</li>' +
                         '</ul>',
     		link: function(scope) {
-                scope.equipment = dataservice.getEquipment().then(function(data) {
-                    return data;
-                });
 
-                scope.$watch("equipment", function(newVal, oldVal){ 
-                    showSelected(newVal);
-                }, true);
-
-                function showSelected(list) {
-                    if(!(list.length)) {
-                        return;
-                    }
-                    var foundIndex = 0;
-
-                    foundIndex = list.findIndex(function(item) {
-                        return item.selected === true;
-                    }) 
-
-                    scope.selectedItem = list[foundIndex]
-                }
             }
     	};
 
--- client/app/components/shop/shop-itemlist.directive.js
+++ client/app/components/shop/shop-itemlist.directive.js
@@ -2,30 +2,18 @@
   'use strict';
 
   angular.module('App.components.shop')
-    .directive('itemlist', function (dataservice, $q) {
+    .directive('itemlist', function (dataservice) {
 
     	var ddo = {
     		template: '<h4>item list</h4>' +
     					'<span ng-show="!equipment.length > 0" class="glyphicon glyphicon-refresh glyphicon-spin"></span>' +
   						'<ul class="list-unstyled">' +
-					        '<li ng-repeat="item in equipment"><a ng-class="{active: item.selected===true}" class="text-info" href="" ng-click="selectItem(equipment, item)">{{item.name}}</a></li>' +
+					        '<li ng-repeat="item in equipment"><a>{{item.name}}</a></li>' +
 					    '</ul>',
     		link: function(scope) {
-    			
-    			scope.equipment = dataservice.getEquipment();
-    			scope.selectItem = selectItem;
-
-    			$q.when(scope.equipment).then(function(data) {
-    				scope.equipment = data;
-    				selectItem(scope.equipment, scope.equipment[0]);
-    			})
-
-    			function selectItem(list, item) {
-    				list.forEach(function(i) {
-    					i['selected'] = false;
-    				});
-    				item['selected'] = true;
-    			}
+    			dataservice.getEquipment().then(function(data) {
+                    scope.equipment = data;
+                });
     		}
     	};
 
